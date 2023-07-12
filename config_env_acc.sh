9e405244edbf650a1f6e45d9b784f3f1cfce06a6
Barza Nisar <barzanisar93@gmail.com>
2023-07-12 12:24:49 -0400

    bug fix

--- config_env_acc.sh
+++ config_env_acc.sh
@@ -2,12 +2,12 @@
 
 module load python
 
-virtualenv --no-download $SLURM_TMPDIR/ENV
+virtualenv --no-download $SLURM_TMPDIR/env
 
-source $SLURM_TMPDIR/ENV/bin/activate
+source $SLURM_TMPDIR/env/bin/activate
 
 pip install --upgrade pip --no-index
 
 pip install --no-index torchvision accelerate
 
-echo "Done installing virtualenv!"
+echo "Done installing virtualenv at $SLURM_TMPDIR/env !"
